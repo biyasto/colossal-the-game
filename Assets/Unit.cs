1693e001b8a4aa0df6be5a79f3aea6e135733183
biyasto <biyasto@gmail.com>
2022-06-16 00:24:46 +0700

    Refactor Struct

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -1,107 +1,182 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Mirror;
 using UnityEngine;
-using UnityEngine.Timeline;
+using Mirror;
+using Mirror.Examples.Basic;
 
 public class Unit : NetworkBehaviour
 {
+   [SyncVar]
+    public string unitName;
+    [SyncVar]
+    public int maxHp;
+    
+    [SyncVar(hook = nameof(UnitHpChanged))]
+    public int currentHp;
+    [SyncVar(hook = nameof(UnitAtkChanged))]
+    public int atk;
+    [SyncVar(hook = nameof(UnitPrtChanged))]
+    public int prt;
+    [SyncVar(hook = nameof(UnitTtdChanged))]
+    public int ttd;
+   
+    [SyncVar]
+    public Unit emeny;
+    [SyncVar]
+    public bool isResetTtd;
 
-	public MoveSet moveset;
-	[SyncVar] public string unitName;
+    private const int MAX_VALUE_PRT = 80;
+    private const int MIN_VALUE_PRT = 0;
+    private const int MIN_VALUE_ATK = 0;
 
-	[SyncVar]public int atk;
+    public event System.Action<int> OnUnitAtkChanged;
+    public event System.Action<int> OnUnitHpChanged;
+    public event System.Action<int> OnUnitTtdChanged;
+    public event System.Action<int> OnUnitPrtChanged;
 
-	[SyncVar]public int maxHp;
-	[SyncVar]public int currentHp;
+    public Player playerObj;
+    void UnitAtkChanged(int _, int newNumber)
+    {
+        OnUnitAtkChanged?.Invoke(newNumber);
+    }
+    void UnitHpChanged(int _, int newNumber)
+    {
+        OnUnitHpChanged?.Invoke(newNumber);
+    }
+    void UnitTtdChanged(int _, int newNumber)
+    {
+        OnUnitTtdChanged?.Invoke(newNumber);
+    }
+    void UnitPrtChanged(int _, int newNumber)
+    {
+        OnUnitPrtChanged?.Invoke(newNumber);
+    }
+    private void Start()
+    {
+       // moveset = GetComponent<MoveSet>();
+    }
+    public override void OnStartClient()
+    {
+        Debug.Log("OnStartClient");
+
+        // Instantiate the player UI as child of the Players Panel
+        /*playerUIObject = Instantiate(playerUIPrefab, CanvasUI.instance.playersPanel);
+        playerUI = playerUIObject.GetComponent<PlayerUI>(); */
+
+        // wire up all events to handlers in PlayerUI
+        OnUnitHpChanged = playerObj.playerUI.OnUnitHpChanged;
+        OnUnitAtkChanged = playerObj.playerUI.OnUnitAtkChanged;
+        OnUnitPrtChanged = playerObj.playerUI.OnUnitPrtChanged;
+        OnUnitTtdChanged = playerObj.playerUI.OnUnitTtdChanged;
+        // Invoke all event handlers with the initial data from spawn payload
+        OnUnitHpChanged.Invoke(currentHp);
+        OnUnitAtkChanged.Invoke(atk);
+        OnUnitPrtChanged.Invoke(prt);
+        OnUnitTtdChanged.Invoke(ttd);
+    }
+   
+    public void TakeDamage(int amount)
+    {
+        if (amount > 0)
+            currentHp -= (int) (1.0 * amount * (100 - prt) / 100);
+        else
+            currentHp -= amount;
+        ReceiveTtd((int) (1.0 * amount * (100 - prt) / 100));
+        isResetTtd = false;
+        if (currentHp <= 0)
+        {
+            currentHp = 0;
+
+        }
+
+    }
+
+    private uint GetEnemyNetID()
+    {
+        if (netId == Player.Player1NetID) return Player.Player2NetID;
+        if (netId == Player.Player2NetID) return Player.Player1NetID;
+        else return 0;
+    }
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            DamageEnemy(1, GetEnemyNetID());
+        }
+    }
+
+    [Command]
+    public void DamageEnemy(int amount, uint id)
+    {
+        RPCEnemyTakeDamage(10,id);
+    }
+    [ClientRpc]
+    public void RPCEnemyTakeDamage(int amount,uint playerId)
+    {
+        if (playerId == 0) return;
+        NetworkIdentity.spawned[playerId].gameObject.GetComponent<Unit>().atk+=amount;
+        NetworkIdentity.spawned[playerId].gameObject.GetComponent<Player>().playerUI
+            .OnUnitAtkChanged(NetworkIdentity.spawned[playerId].gameObject.GetComponent<Unit>().atk);
+    }
+    
+    [ClientRpc]
+    public void RPCReceiveTtd(int amount)
+    {
+        ttd += amount;
+    }
+    public void ResetTtd()
+    {
+        ttd = 0;
+    }
+    
+    public void SetHp(int amount)
+    {
+        currentHp = amount > maxHp ? maxHp : amount;
+    }
+    [Command]
+    public void ReceiveTtd(int amount)
+    {
+        RPCReceiveTtd(amount);
+    }
+    public void SetPrt(int amount)
+    {
+        ChangePrt(-prt);
+        ChangePrt(amount);
+    }
+
+    public void SetAtk(int amount)
+    {
+        ChangeAtk(-atk);
+        ChangeAtk(amount);
+    }
+  
+    public void ChangePrt(int amount)
+    {
+        prt += amount;
+        if (prt < MIN_VALUE_PRT) prt = MIN_VALUE_PRT;
+        if (prt > MAX_VALUE_PRT) prt = MAX_VALUE_PRT;
+    }
+   
+    public void ChangeAtk(int amount)
+    {
+        atk += amount;
+        if (prt < MIN_VALUE_ATK) prt = MIN_VALUE_ATK;
+    }
+    
+    public void Heal(int amount)
+    {
+        if (amount <= 0) return;
+        currentHp += amount;
+        if (currentHp > maxHp)
+            currentHp = maxHp;
+    }
 	
-	[SyncVar]public int prt;
-	[SyncVar]public int ttd;
+ 
+    public void SetEnemy(Unit e)
+    {
+        emeny = e;
+    }
 
-	
-	[SyncVar]public Unit emeny; 
-	[SyncVar]public bool isResetTtd;
-
-	private const int MAX_VALUE_PRT = 80;
-	private const int MIN_VALUE_PRT = 0;
-	private const int MIN_VALUE_ATK = 0;
-
-
-	private void Start()
-	{
-		moveset = GetComponent<MoveSet>();
-	}
-	[ClientRpc]
-	public void TakeDamage(int amount)
-	{
-		if (amount > 0)
-			currentHp -= (int) (1.0 * amount * (100 - prt) / 100);
-		else
-			currentHp -= amount;
-		ReceiveTtd((int) (1.0 * amount * (100 - prt) / 100));
-		isResetTtd = false;
-		if (currentHp <= 0)
-		{
-			currentHp = 0;
-			
-		}
-
-		
-	}
-	[ClientRpc]
-	public void ReceiveTtd(int amount)
-	{
-		ttd += amount;
-	}
-	[ClientRpc]
-	public void ResetTtd()
-	{
-		ttd = 0;
-	}
-	[ClientRpc]
-	public void SetHp(int amount)
-	{
-		currentHp = amount > maxHp ? maxHp : amount;
-	}
-	[ClientRpc]
-	public void SetPrt(int amount)
-	{
-		ChangePrt(-prt);
-		ChangePrt(amount);
-	}
-	[ClientRpc]
-	public void SetAtk(int amount)
-	{
-		ChangeAtk(-atk);
-		ChangeAtk(amount);
-	}
-	[ClientRpc]
-	public void ChangePrt(int amount)
-	{
-		prt += amount;
-		if (prt < MIN_VALUE_PRT) prt = MIN_VALUE_PRT;
-		if (prt > MAX_VALUE_PRT) prt = MAX_VALUE_PRT;
-	}
-	[ClientRpc]
-	public void ChangeAtk(int amount)
-	{
-		atk += amount;
-		if (prt < MIN_VALUE_ATK) prt = MIN_VALUE_ATK;
-	}
-	[ClientRpc]
-	public void Heal(int amount)
-	{
-		if (amount <= 0) return;
-		currentHp += amount;
-		if (currentHp > maxHp)
-			currentHp = maxHp;
-	}
-	
-	[ClientRpc]
-	public void SetEnemy(Unit e)
-	{
-		emeny = e;
-	}
-	
+    
 }
