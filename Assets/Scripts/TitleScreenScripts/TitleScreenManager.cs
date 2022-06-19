e2ecf06b17c65248d600131af9054d473cbf525d
TranNguyenTienNam <trannguyentiennamuit@gmail.com>
2022-06-19 10:15:45 +0700

    Choose character for battle

--- Assets/Scripts/TitleScreenScripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenScripts/TitleScreenManager.cs
@@ -15,6 +15,7 @@ public class TitleScreenManager : MonoBehaviour
     [SerializeField] private GameObject PlayerNamePanel;
     [SerializeField] private GameObject HostOrJoinPanel;
     [SerializeField] private GameObject EnterIPAddressPanel;
+    [SerializeField] private GameObject ChooseCharacterPanel;
 
     [Header("PlayerName UI")]
     [SerializeField] private TMP_InputField playerNameInputField;
@@ -25,13 +26,29 @@ public class TitleScreenManager : MonoBehaviour
     [Header("Misc. UI")]
     [SerializeField] private Button returnToMainMenu;
 
+    [Header("Choose Character")]
+    [SerializeField] private Image curCharacter;
+    [SerializeField] private Image preCharacter;
+    [SerializeField] private Image nextCharacter;
+
+    [SerializeField] private Sprite defaultCharacter;
+    [SerializeField] private float localScale = 0.5f;
+    [SerializeField] private float defaultAlpha = 0.55f;
+
+    [SerializeField] TMP_Text characterName;
+
     private const string PlayerPrefsNameKey = "PlayerName";
+    private List<Character> characters = new List<Character>();
+    private int currentCharacterIndex = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
         MakeInstance();
         ReturnToMainMenu();
+
+        characters = Inventory.ownedCharacters;
+        SetCharacterUI(currentCharacterIndex);
     }
 
     // Update is called once per frame
@@ -47,6 +64,7 @@ public class TitleScreenManager : MonoBehaviour
     public void ReturnToMainMenu()
     {
         mainMenuPanel.SetActive(true);
+        ChooseCharacterPanel.SetActive(true);
         PlayerNamePanel.SetActive(false);
         HostOrJoinPanel.SetActive(false);
         EnterIPAddressPanel.SetActive(false);
@@ -56,6 +74,7 @@ public class TitleScreenManager : MonoBehaviour
     {
         //SceneManager.LoadScene("Gameplay");
         mainMenuPanel.SetActive(false);
+        ChooseCharacterPanel.SetActive(false);
         PlayerNamePanel.SetActive(true);
         GetSavedPlayerName();
         returnToMainMenu.gameObject.SetActive(true);
@@ -104,5 +123,77 @@ public class TitleScreenManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void ChooseNextCharacter()
+    {
+        if (currentCharacterIndex < characters.Count - 1)
+        {
+            currentCharacterIndex++;
+            SetCharacterUI(currentCharacterIndex);
+        }
+    }
+
+    public void ChoosePreCharacter()
+    {
+        if (currentCharacterIndex > 0)
+        {
+            currentCharacterIndex--;
+            SetCharacterUI(currentCharacterIndex);
+        }
+    }
+
+    public void SetCharacterUI(int characterIndex)
+    {
+        if (characters.Count == 0)
+        {
+            SetDefaultCharacter(preCharacter);
+            SetDefaultCharacter(curCharacter);
+            SetDefaultCharacter(nextCharacter);
+            return;
+        }
+
+        var character = characters[characterIndex];
+
+        characterName.text = character.characterName;
+
+        curCharacter.sprite = character.sprite;
+
+        if (characterIndex != 0)
+        {
+            preCharacter.sprite = characters[characterIndex - 1].sprite;
+            preCharacter.transform.localScale = new Vector3(1, 1, 1);
+
+            var tempColor = preCharacter.color;
+            tempColor.a = defaultAlpha;
+            preCharacter.color = tempColor;
+        }
+        else
+        {
+            SetDefaultCharacter(preCharacter);
+        }
+
+        if (characterIndex != characters.Count - 1)
+        {
+            nextCharacter.sprite = characters[characterIndex + 1].sprite;
+            nextCharacter.transform.localScale = new Vector3(1, 1, 1);
+
+            var tempColor = nextCharacter.color;
+            tempColor.a = defaultAlpha;
+            nextCharacter.color = tempColor;
+        }
+        else
+        {
+            SetDefaultCharacter(nextCharacter);
+        }
+    }
+    private void SetDefaultCharacter(Image image)
+    {
+        image.sprite = defaultCharacter;
+        image.transform.localScale = new Vector2(localScale, localScale);
+
+        var tempColor = image.color;
+        tempColor.a = 1.0f;
+        image.color = tempColor;
     }
 }
